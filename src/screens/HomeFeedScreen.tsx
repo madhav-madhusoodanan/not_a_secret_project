ac0173f563d435c83641a9b4cc58547468ee3771
Devansh Agarwal <devansh4m@gmail.com>
2021-07-31 01:20:15 +0530

    importing SlackClone structure

--- src/screens/HomeFeedScreen.tsx
+++ src/screens/HomeFeedScreen.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Button, Text} from 'react-native-paper';
 
-export default function Home() {
+export default function HomeFeedScreen() {
   return (
     <View>
       <Button
