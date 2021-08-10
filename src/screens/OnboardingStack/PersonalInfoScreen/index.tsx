958edea1c4709662eca63b12801eb0f91d58871d
Devansh Agarwal <devansh4m@gmail.com>
2021-08-10 23:13:30 +0530

    still working on gender page

--- src/screens/OnboardingStack/PersonalInfoScreen/index.tsx
+++ src/screens/OnboardingStack/PersonalInfoScreen/index.tsx
@@ -1,94 +1,38 @@
-import * as React from 'react';
-import {Pressable, View} from 'react-native';
+import React, {useState, useRef, useEffect} from 'react';
+import {View} from 'react-native';
 import {Headline} from 'react-native-paper';
-import {View as MView} from 'moti';
-import {Easing} from 'react-native-reanimated';
 
 import styles from './styles';
+import GenderSwitch from './../../../components/GenderSwitch';
+import NavButton from '../../../components/Buttons/NavigationButton';
 
-const SIZE = 60;
-const TRACK_SIZE = SIZE * 1.5;
-const TRACK_HEIGHT = SIZE * 0.4;
-const transition = {
-  type: 'timing',
-  duration: 300,
-  easing: Easing.inOut(Easing.ease),
-};
-const Switch = ({size = SIZE, onPress, isActive}) => {
-  return (
-    <Pressable onPress={onPress}>
-      <View style={{alignItems: 'center', justifyContent: 'center'}}>
-        <MView
-          from={{backgroundColor: isActive ? '#2C2C2C' : '#DCDCDC'}}
-          animate={{backgroundColor: isActive ? '#2C2C2C' : '#DCDCDC'}}
-          transition={transition}
-          style={{
-            position: 'absolute',
-            width: TRACK_SIZE,
-            height: TRACK_HEIGHT,
-            borderRadius: TRACK_HEIGHT,
-          }}
-        />
-        <MView
-          transition={transition}
-          from={{
-            translateX: isActive ? TRACK_SIZE / 4 : -TRACK_SIZE / 4,
-          }}
-          animate={{
-            translateX: isActive ? TRACK_SIZE / 4 : -TRACK_SIZE / 4,
-          }}
-          style={{
-            width: SIZE,
-            height: SIZE,
-            borderRadius: SIZE,
-            backgroundColor: '#fff',
-            alignItems: 'center',
-            justifyContent: 'center',
-          }}>
-          <MView
-            transition={transition}
-            from={{
-              width: isActive ? 0 : SIZE * 0.6,
-              borderColor: isActive ? '#2C2C2C' : '#DCDCDC',
-            }}
-            animate={{
-              width: isActive ? 0 : SIZE * 0.6,
-              borderColor: isActive ? '#2C2C2C' : '#DCDCDC',
-            }}
-            style={{
-              width: SIZE * 0.6,
-              height: SIZE * 0.6,
-              borderRadius: SIZE * 0.6,
-              borderWidth: SIZE * 0.1,
-            }}
-          />
-        </MView>
-      </View>
-    </Pressable>
-  );
-};
+import {useNavigation} from '@react-navigation/native';
+import {SafeAreaView} from 'react-native-safe-area-context';
+
 export default function PersonalInfoScreen() {
   const [isActive, setIsActive] = React.useState(false);
+  const {navigate, goBack} = useNavigation();
+  const submitHandler = () => {
+    navigate('ProfilePhotoScreen');
+  };
 
   return (
-    <View
-      style={{
-        flex: 1,
-        alignItems: 'center',
-        justifyContent: 'center',
-        backgroundColor: '#F3F3F4',
-      }}>
+    <SafeAreaView style={styles.container}>
+      <Headline numberOfLines={2} style={styles.title}>
+        Select Gender
+      </Headline>
       <Headline
         numberOfLines={2}
-        style={[styles.title, {color: isActive ? 'deeppink' : 'dodgerblue'}]}>
-        Boy
+        style={[styles.gender, {color: isActive ? 'deeppink' : 'dodgerblue'}]}>
+        Male
       </Headline>
-      <Switch
+      <GenderSwitch
         isActive={isActive}
         onPress={() => {
           setIsActive(isActive => !isActive);
         }}
       />
-    </View>
+      <NavButton onPress={submitHandler} text="Confirm" />
+    </SafeAreaView>
   );
 }
--- src/screens/OnboardingStack/PersonalInfoScreen/styles.tsx
+++ src/screens/OnboardingStack/PersonalInfoScreen/styles.tsx
@@ -6,16 +6,27 @@ const styles = StyleSheet.create({
     flex: 1,
     minHeight: 800,
     paddingTop: 14,
+    // alignItems: 'center',
+    // justifyContent: 'center',
     paddingHorizontal: 20,
     backgroundColor: Theme.colors.background,
   },
   title: {
     marginTop: 50,
     lineHeight: 36,
+    color: '#000',
     fontSize: 28,
     textAlign: 'left',
     fontFamily: 'Gilroy-Bold',
     marginBottom: 100,
+  },
+  gender: {
+    marginTop: 50,
+    lineHeight: 36,
+    fontSize: 28,
+    textAlign: 'center',
+    fontFamily: 'Gilroy-Bold',
+    marginBottom: 40,
   },
 });
 
