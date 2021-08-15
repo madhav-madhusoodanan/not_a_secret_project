cac41e2b40b4531cb20f0b933c4bbc60bd5e5e06
Devansh Agarwal <devansh4m@gmail.com>
2021-08-16 00:11:23 +0530

    reduced concurrent data points

--- src/components/CommunityFeed/FeedScene.tsx
+++ src/components/CommunityFeed/FeedScene.tsx
@@ -14,7 +14,7 @@ import Post from '../Post';
 
 type ScrollEvent = NativeSyntheticEvent<NativeScrollEvent>;
 
-const data = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15].map(i => ({
+const data = [1, 2, 3, 4, 5].map(i => ({
   title: `Title ${i}`,
   key: `key-${i}`,
 }));
