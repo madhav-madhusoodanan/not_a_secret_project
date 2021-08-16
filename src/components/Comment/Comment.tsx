f6d233e5d6ce5ab9f0d943a1ea10b2450630f74a
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2021-08-16 11:42:08 +0530

    converted class to hooks

--- src/components/Comment/Comment.tsx
+++ src/components/Comment/Comment.tsx
@@ -1,100 +1,62 @@
-import React, {Component} from 'react';
-
+import React, {useEffect, useState} from 'react';
 import {View, Text, Image, TouchableOpacity} from 'react-native';
-
-import PropTypes from 'prop-types';
-
 import commentStyling from './CommentStyling';
-
 import Reply from './Reply';
 
-export default class Comment extends Component {
-  state = {
-    liked: false,
+const Comment = ({displayLike, commentData}) => {
+  const [liked, setLiked] = useState(false);
+  const {content, ownerName, ownerAvatar, replies, date} = commentData;
+  useEffect(() => {
+    setLiked(displayLike);
+  }, []);
+
+  const onPressReply = () => {};
+
+  const onPressLike = () => {
+    setLiked(!liked);
   };
-
-  componentDidMount = () => {
-    const {displayLike} = this.props;
-    this.setState({
-      liked: displayLike,
-    });
-  };
-
-  onPressReply = () => {};
-
-  onPressLike = () => {
-    this.setState({
-      liked: !this.state.liked,
-    });
-  };
-
-  renderReplies = replies => {
+  const renderReplies = replies => {
     return replies.map((reply, index) => (
       <View key={index}>
+        {/* @ts-ignore */}
         <Reply reply={reply} />
       </View>
     ));
   };
-
-  render() {
-    const {commentData} = this.props;
-    const {content, ownerName, ownerAvatar, replies, date} = commentData;
-    const {liked} = this.state;
-
-    return (
-      <View style={{backgroundColor: 'white'}}>
-        <View style={commentStyling.mainWrapper}>
-          <Image style={commentStyling.image} source={{uri: ownerAvatar}} />
-          <View style={commentStyling.wrapper}>
-            <View style={commentStyling.commentHeader}>
-              <Text style={commentStyling.commentOwner}>{ownerName}</Text>
-              <Text style={commentStyling.commentDate}>{date}</Text>
-            </View>
-            <Text style={commentStyling.content}>{content}</Text>
+  return (
+    <View style={{backgroundColor: 'white'}}>
+      <View style={commentStyling.mainWrapper}>
+        <Image style={commentStyling.image} source={{uri: ownerAvatar}} />
+        <View style={commentStyling.wrapper}>
+          <View style={commentStyling.commentHeader}>
+            <Text style={commentStyling.commentOwner}>{ownerName}</Text>
+            <Text style={commentStyling.commentDate}>{date}</Text>
           </View>
+          <Text style={commentStyling.content}>{content}</Text>
         </View>
-        <View style={commentStyling.commentFooter}>
-          {liked ? (
-            <TouchableOpacity onPress={this.onPressLike}>
-              <View style={{paddingRight: 5, paddingLeft: 5}}>
-                <Text style={commentStyling.footerElementLiked}>Like</Text>
-              </View>
-            </TouchableOpacity>
-          ) : (
-            <TouchableOpacity onPress={this.onPressLike}>
-              <View style={{paddingRight: 5, paddingLeft: 5}}>
-                <Text style={commentStyling.footerElement}>Like</Text>
-              </View>
-            </TouchableOpacity>
-          )}
-
-          <TouchableOpacity onPress={this.onPress}>
-            <Text style={commentStyling.footerElement}>Reply</Text>
-          </TouchableOpacity>
-        </View>
-        {replies ? this.renderReplies(replies) : null}
       </View>
-    );
-  }
-}
+      <View style={commentStyling.commentFooter}>
+        {liked ? (
+          <TouchableOpacity onPress={onPressLike}>
+            <View style={{paddingRight: 5, paddingLeft: 5}}>
+              <Text style={commentStyling.footerElementLiked}>Like</Text>
+            </View>
+          </TouchableOpacity>
+        ) : (
+          <TouchableOpacity onPress={onPressLike}>
+            <View style={{paddingRight: 5, paddingLeft: 5}}>
+              <Text style={commentStyling.footerElement}>Like</Text>
+            </View>
+          </TouchableOpacity>
+        )}
 
-Comment.propTypes = {
-  commentData: PropTypes.shape({
-    ownerName: PropTypes.string.isRequired,
-    ownerAvatar: PropTypes.string,
-    content: PropTypes.string.isRequired,
-    date: PropTypes.string,
-    liked: PropTypes.bool,
-    replies: PropTypes.arrayOf(
-      PropTypes.shape({
-        ownerName: PropTypes.string,
-        ownerAvatar: PropTypes.string,
-        content: PropTypes.string.isRequired,
-        date: PropTypes.string,
-      }),
-    ),
-  }),
-  backgroundColor: PropTypes.string,
-  textColor: PropTypes.string,
-  displayLike: PropTypes.bool,
+        <TouchableOpacity onPress={onPressReply}>
+          <Text style={commentStyling.footerElement}>Reply</Text>
+        </TouchableOpacity>
+      </View>
+      {replies ? renderReplies(replies) : null}
+    </View>
+  );
 };
+
+export default Comment
