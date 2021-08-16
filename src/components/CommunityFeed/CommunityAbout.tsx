ca899710a497349c4bb9e75de5fdf6728e8717c2
Devansh Agarwal <devansh4m@gmail.com>
2021-08-17 01:45:11 +0530

    Update CommunityAbout.tsx

--- src/components/CommunityFeed/CommunityAbout.tsx
+++ src/components/CommunityFeed/CommunityAbout.tsx
@@ -1,41 +1,55 @@
 import React from 'react';
 import {RefreshControl, StyleSheet, Text, View} from 'react-native';
-import {ComAnimatedTabView} from '../Library';
+import {
+  Button,
+  Colors,
+  Card,
+  List,
+  Subheading,
+  Title,
+} from 'react-native-paper';
 
-const CommunityAbout = ({
-  styles,
-  isActive,
-  routeKey,
-  scrollY,
-  trackRef,
-  onMomentumScrollBegin,
-  onMomentumScrollEnd,
-  onScrollEndDrag,
-  data,
-}) => {
+const CommunityAbout = () => {
   return (
-    <ComAnimatedTabView
-      data={data}
-      windowSize={3}
-      refreshControl={
-        <RefreshControl refreshing={false} onRefresh={() => null} />
-      }
-      renderItem={({item}: any) => (
-        <View style={styles.container}>
-          <View style={styles.inner}>
-            <Text>{item.key}</Text>
-            {/* <Post allowed /> */}
-          </View>
-        </View>
-      )}
-      onRef={(ref: any) => {
-        trackRef(routeKey, ref);
-      }}
-      scrollY={isActive ? scrollY : undefined}
-      onScrollEndDrag={onScrollEndDrag}
-      onMomentumScrollBegin={onMomentumScrollBegin}
-      onMomentumScrollEnd={onMomentumScrollEnd}
-    />
+    <Card
+      style={{
+        borderRadius: 12,
+        width: '88%',
+        alignSelf: 'center',
+        // backgroundColor: '#F8F8F9',
+        elevation: 10,
+        marginTop: 120,
+      }}>
+      <Title style={{color: '#000', paddingLeft: 15, paddingVertical: 10}}>
+        Group Details
+      </Title>
+      <Subheading style={{color: 'gray', paddingLeft: 15}}>Created</Subheading>
+      <List.Item
+        title="13 October, 2021 by Aman Dhattarwal"
+        titleStyle={{color: '#000'}}
+        style={{
+          height: 50,
+          justifyContent: 'center',
+          // marginBottom: 10,
+        }}
+        left={() => (
+          <List.Icon icon="clock-time-four-outline" color={Colors.black} />
+        )}
+      />
+      <Subheading style={{color: 'gray', paddingLeft: 15}}>
+        Privacy settings
+      </Subheading>
+      <List.Item
+        title="Private"
+        titleStyle={{color: '#000'}}
+        style={{
+          height: 50,
+          justifyContent: 'center',
+          // marginBottom: 10,
+        }}
+        left={() => <List.Icon icon="lock-outline" color={Colors.black} />}
+      />
+    </Card>
   );
 };
 
