4bff6670bf2a226c9e8f7c2572cc6213d9f600b1
Devansh Agarwal <devansh4m@gmail.com>
2021-08-17 13:13:05 +0530

    working on settings ui

--- src/screens/ProfileScreens/SettingsScreen/index.tsx
+++ src/screens/ProfileScreens/SettingsScreen/index.tsx
@@ -1,5 +1,7 @@
 import React from 'react';
 import {StyleSheet, Text, View} from 'react-native';
+import {List} from 'react-native-paper';
+import RNBounceable from '@freakycoder/react-native-bounceable';
 import {SafeAreaView} from 'react-native-safe-area-context';
 
 import styles from './styles';
@@ -7,7 +9,32 @@ import styles from './styles';
 export default function index() {
   return (
     <SafeAreaView style={styles.container}>
-      <Text style={{color: 'black'}}>Settings UI</Text>
+      <View style={styles.listGroup}>
+        <RNBounceable bounceEffect={0.97} onPress={() => {}}>
+          <List.Item
+            title="Privacy Policy"
+            style={styles.listItem}
+            right={() => <List.Icon color="#000" icon="chevron-right" />}
+            left={() => <List.Icon color="#45E5A2" icon="folder" />}
+          />
+        </RNBounceable>
+        <RNBounceable bounceEffect={0.97} onPress={() => {}}>
+          <List.Item
+            title="Report Bug"
+            style={styles.listItem}
+            right={() => <List.Icon color="#000" icon="chevron-right" />}
+            left={() => <List.Icon color="#71A1FF" icon="bug" />}
+          />
+        </RNBounceable>
+        <RNBounceable bounceEffect={0.97} onPress={() => {}}>
+          <List.Item
+            title="Logout"
+            style={styles.listItem}
+            right={() => <List.Icon color="#000" icon="chevron-right" />}
+            left={() => <List.Icon color="#FF5E5E" icon="folder" />}
+          />
+        </RNBounceable>
+      </View>
     </SafeAreaView>
   );
 }
--- src/screens/ProfileScreens/SettingsScreen/styles.tsx
+++ src/screens/ProfileScreens/SettingsScreen/styles.tsx
@@ -4,10 +4,24 @@ import {Theme} from '../../../constants/appTheme';
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    minHeight: 800,
-    paddingTop: 54,
-    paddingHorizontal: 20,
+    paddingTop: 64,
+    // paddingHorizontal: 20,
     backgroundColor: Theme.colors.background,
+  },
+  listGroup: {
+    marginHorizontal: 10,
+    marginTop: 20,
+    // alignSelf: 'center',
+    flexDirection: 'column',
+  },
+  listItem: {
+    backgroundColor: '#f5f3f4',
+    borderRadius: 12,
+    marginVertical: 10,
+    height: 60,
+    width: '100%',
+    // alignItems: 'center',
+    justifyContent: 'center',
   },
 });
 
