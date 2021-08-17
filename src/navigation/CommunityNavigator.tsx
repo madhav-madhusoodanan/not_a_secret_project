d68c2bb74c81dff74fb1e14f69c1708d15f2a2f5
Devansh Agarwal <devansh4m@gmail.com>
2021-08-17 23:28:42 +0530

    removed shit

--- src/navigation/CommunityNavigator.tsx
+++ src/navigation/CommunityNavigator.tsx
@@ -8,55 +8,44 @@ import {CommunityScreens} from '../screens/CommunityScreens';
 const Community = createMaterialTopTabNavigator();
 const CommunityNavigator = () => {
   return (
-    <View
-      style={{
-        flex: 1,
-        backgroundColor: 'red',
-        flexDirection: 'column',
-      }}>
-      <View>
-        <Text style={{textAlign: 'center'}}>Top Text on Tabs</Text>
-      </View>
-      {/* <CommunityHeader/> */}
-      <Community.Navigator
-        screenOptions={{
-          tabBarBounces: true,
-          tabBarIconStyle: {top: 2},
-          tabBarLabelStyle: {fontSize: 14, fontFamily: 'Inter-Bold'},
-          tabBarStyle: {
-            backgroundColor: 'white',
-          },
-          tabBarItemStyle: {
-            flexDirection: 'row',
-            alignItems: 'center',
-            padding: 10,
-          },
-          tabBarIndicatorStyle: {
-            backgroundColor: 'black',
-            borderBottomWidth: 3,
-          },
-        }}
-        initialRouteName="FeedScreen">
-        <Community.Screen
-          options={{
-            title: 'Feed',
-            lazy: true,
+    <Community.Navigator
+      screenOptions={{
+        tabBarBounces: true,
+        tabBarIconStyle: {top: 2},
+        tabBarLabelStyle: {fontSize: 14, fontFamily: 'Inter-Bold'},
+        tabBarStyle: {
+          backgroundColor: 'white',
+        },
+        tabBarItemStyle: {
+          flexDirection: 'row',
+          alignItems: 'center',
+          padding: 10,
+        },
+        tabBarIndicatorStyle: {
+          backgroundColor: 'black',
+          borderBottomWidth: 3,
+        },
+      }}
+      initialRouteName="FeedScreen">
+      <Community.Screen
+        options={{
+          title: 'Feed',
+          lazy: true,
 
-            tabBarIcon: () => <FeedIcon size="20" color="#000" />,
-          }}
-          name="FeedScreen"
-          component={CommunityScreens.FeedScreen}
-        />
-        <Community.Screen
-          options={{
-            title: 'About',
-            tabBarIcon: () => <AboutIcon size="20" color="#000" />,
-          }}
-          name="AboutScreen"
-          component={CommunityScreens.AboutScreen}
-        />
-      </Community.Navigator>
-    </View>
+          tabBarIcon: () => <FeedIcon size="20" color="#000" />,
+        }}
+        name="FeedScreen"
+        component={CommunityScreens.FeedScreen}
+      />
+      <Community.Screen
+        options={{
+          title: 'About',
+          tabBarIcon: () => <AboutIcon size="20" color="#000" />,
+        }}
+        name="AboutScreen"
+        component={CommunityScreens.AboutScreen}
+      />
+    </Community.Navigator>
   );
 };
 
