e165e771643d5cdd6bb548830bd7bb85f5742ef2
MehtaManan07 <f20190507@pilani.bits-pilani.ac.in>
2021-08-25 01:16:05 +0530

    made previous written code readable

---
+++ src/components/Profile/ProfileTop.tsx
@@ -0,0 +1,14 @@
+import React from 'react'
+import { StyleSheet, Text, View } from 'react-native'
+
+const EditProfileForm = () => {
+    return (
+        <View>
+            <Text></Text>
+        </View>
+    )
+}
+
+export default EditProfileForm
+
+const styles = StyleSheet.create({})
--- src/screens/ProfileScreens/EditProfileScreen/index.tsx
+++ src/screens/ProfileScreens/EditProfileScreen/index.tsx
@@ -1,22 +1,13 @@
 import React, {useEffect, useState} from 'react';
-import {
-  KeyboardAvoidingView,
-  Text,
-  ScrollView,
-  View,
-  TouchableOpacity,
-} from 'react-native';
-import {List, Subheading, Colors, Button} from 'react-native-paper';
-import FastImage from 'react-native-fast-image';
-import {TextInput} from 'react-native-gesture-handler';
+import {Text} from 'react-native';
+import {Button} from 'react-native-paper';
 import {SafeAreaView} from 'react-native-safe-area-context';
 import {useSelector, useDispatch} from 'react-redux';
 import styles from './styles';
 import {updateuser} from '../../../store/Actions/AuthActions';
-import ProfileButton from '../../../components/Buttons/ProfileButton';
-import ProfileInput from '../../../components/TextInput/ProfileInput';
 import {useIsFocused} from '@react-navigation/core';
-
+import EditProfileForm from '../../../components/Profile/EditProfileForm';
+import Spinner from '../../../components/Spinner'
 const bioContent =
   '👉 To draw attention attention attention ✈️ Commonly used by travel brands attention 🎥 To call attention to you YouTube channel attention 🥳 To indicate an achievement or award attention';
 
@@ -67,77 +58,16 @@ export default function EditProfileScreen() {
   }, [isFocused]);
   return (
     <SafeAreaView style={styles.container}>
-      {user ? (
-        <KeyboardAvoidingView enabled>
-          <ScrollView
-            showsVerticalScrollIndicator={false}
-            overScrollMode={'never'}
-            style={styles.scrollStyle}>
-            <View style={styles.viewOne}>
-              <View style={styles.viewTwo}>
-                <TouchableOpacity
-                  onPress={() => {}}
-                  activeOpacity={0.5}
-                  style={styles.touchableOpacityOne}>
-                  <List.Icon
-                    color={Colors.black}
-                    icon="pencil"
-                    style={styles.listIconOne}
-                  />
-                </TouchableOpacity>
-                <FastImage
-                  style={styles.imageOne}
-                  source={{
-                    uri: values.avatar,
-                    priority: FastImage.priority.normal,
-                  }}
-                  resizeMode={FastImage.resizeMode.contain}
-                />
-              </View>
-            </View>
-            <ProfileInput
-              label="First Name"
-              value={values.firstName}
-              onChange={val => setValues({...values, firstName: val})}
-            />
-            <ProfileInput
-              label="Last Name"
-              value={values.lastName}
-              onChange={val => setValues({...values, lastName: val})}
-            />
-            <ProfileInput
-              label="Username"
-              onChange={val => setValues({...values, username: val})}
-              value={values.username}
-            />
-            <View style={styles.content}>
-              <Subheading style={styles.subHeading}>Bio</Subheading>
-              <TextInput
-                style={styles.editor}
-                selectionColor={'#c8e7ff'}
-                placeholderTextColor={'#adb5bd'}
-                maxLength={300}
-                multiline
-                textBreakStrategy={'highQuality'}
-                autoCompleteType={'off'}
-                caretHidden={false}
-                autoCapitalize="sentences"
-                keyboardType="default"
-                underlineColorAndroid="#BDBDBD"
-                returnKeyType="next"
-                value={values.bio}
-                onChangeText={val => setValues({...values, bio: val})}
-              />
-            </View>
-            <ProfileButton
-              text="Save"
-              submitHandler={updateHandler}
-              loading={loading}
-            />
-          </ScrollView>
-        </KeyboardAvoidingView>
+      {!user ? (
+        <Spinner />
       ) : (
-        <Text> LOADING </Text>
+        <EditProfileForm
+          loading={loading}
+          setValues={setValues}
+          styles={styles}
+          updateHandler={updateHandler}
+          values={values}
+        />
       )}
     </SafeAreaView>
   );
