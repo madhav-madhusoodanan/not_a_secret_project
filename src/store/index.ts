c22c37e83d1ea38f1e6ccae6fb50d1cc8608c617
Devansh Agarwal <devansh4m@gmail.com>
2021-09-09 23:05:58 +0530

    removed crap code

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,10 +1,10 @@
-import { createStore, applyMiddleware, combineReducers, compose } from "redux";
-import ReduxThunk from "redux-thunk";
-import Reactotron from "../constants/Reactotron";
-import {Auth} from "./Reducers/AuthReducer";
-import {User} from "./Reducers/UserReducer";
-import {Community} from "./Reducers/CommunityReducer";
-import {Post} from "./Reducers/PostReducer";
+import {createStore, applyMiddleware, combineReducers, compose} from 'redux';
+import ReduxThunk from 'redux-thunk';
+import Reactotron from '../constants/Reactotron';
+import {Auth} from './Reducers/AuthReducer';
+import {User} from './Reducers/UserReducer';
+import {Community} from './Reducers/CommunityReducer';
+import {Post} from './Reducers/PostReducer';
 const rootReducer = combineReducers({
   Auth,
   User,
@@ -12,7 +12,7 @@ const rootReducer = combineReducers({
   Post,
 });
 // @ts-ignore
-const createdEnhancer = Reactotron.createEnhancer()
-const middlewares = [ReduxThunk]
-const composed = compose(applyMiddleware(...middlewares), createdEnhancer)
-export const store = createStore(rootReducer, composed)
+const createdEnhancer = Reactotron.createEnhancer();
+const middlewares = [ReduxThunk];
+const composed = compose(applyMiddleware(...middlewares), createdEnhancer);
+export const store = createStore(rootReducer, composed);
