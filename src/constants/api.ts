edea76214a12f179560a3da476a1d296a7c877fa
Devansh Agarwal <devansh4m@gmail.com>
2021-09-09 11:46:20 +0530

    upgraded backward compatible dependencies

--- src/constants/api.ts
+++ src/constants/api.ts
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
 const instance = axios.create({
-  baseURL: 'https://d9ec-219-91-190-171.ngrok.io',
+  baseURL: 'https://1bc3-2400-1a00-b050-3985-f004-b4dc-c6e6-5028.ngrok.io',
 });
 export default instance;
